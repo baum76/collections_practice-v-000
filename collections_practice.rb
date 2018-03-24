ab8f2a43c4fc746d8134917966c0f4f210929bbf
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-24 18:57:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,3 +33,13 @@ def sum_array(array)
   array.each {|num| sum+=num}
   sum
 end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end  
+end
